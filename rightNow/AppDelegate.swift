6bfaa0df395e1ca82f92d99cc4ac6be0c5eef507
sc0tty <slehman4116@gmail.com>
2016-09-24 18:23:45 -0400

    Working initial map

--- rightNow/AppDelegate.swift
+++ rightNow/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSServices.provideAPIKey("AIzaSyA0xy3znV3sUM6sQKJqOkXtT63uEZynRE0")
+        GMSServices.provideAPIKey("AIzaSyDQzX44h1lhpAZ0ZUM6kUwSEnysiCv9Dg0")
         FIRApp.configure()
         
         return true
