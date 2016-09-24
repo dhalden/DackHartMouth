7ae575375357897b0d80b4f0ec8dd782348ad2d3
sc0tty <slehman4116@gmail.com>
2016-09-24 18:04:37 -0400

    map example

--- rightNow/ViewController.swift
+++ rightNow/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let camera = GMSCameraPosition.camera(withLatitude: -33.868,
+                                                          longitude:151.2086, zoom:6)
+        let rect = CGRect(x: 0, y: 0, width: 0, height: 0)
+        let mapView = GMSMapView.map(withFrame: rect, camera:camera)
+        
+        let marker = GMSMarker()
+        marker.position = camera.target
+        marker.snippet = "Hello World"
+        marker.appearAnimation = kGMSMarkerAnimationPop
+        marker.map = mapView
+        
+        self.view = mapView
         // Do any additional setup after loading the view, typically from a nib.
     }
 
